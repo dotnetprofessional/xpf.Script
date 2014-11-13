62256896eb87459e92a623b2e8e9656948ceae31
dnp <dnp@dotnetprofessional.com>
2014-11-12 21:52:50 -0800

    Bug #6 - removing transforms from main NuGet and adding a new NuGet that only adds the configuration. As configuration only needs to be once per installation and only in certain projects not all.

--- nugetbuild.bat
+++ nugetbuild.bat
@@ -7,3 +7,8 @@ cd xpf.Script.SQLServer
 ..\.nuget\nuget.exe pack
 xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
 cd ..
+
+cd xpf.Script.SQLServer.Config
+..\.nuget\nuget.exe pack xpf.Script.SQLServer.Config.nuspec
+xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
+cd ..
