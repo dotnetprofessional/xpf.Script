67397c515cc94e95a9a7942999fe292db42c7154
dnp <garrymc@dotnetprofessional.com>
2016-06-14 20:28:52 -0700

    Updating version ready for public release

--- xpf.Script.SQLServer/Properties/AssemblyInfo.cs
+++ xpf.Script.SQLServer/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0.5")]
-[assembly: AssemblyInformationalVersion("0.2.0.5")] // Used by NuGet
+[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyInformationalVersion("0.2.1.0")] // Used by NuGet
 
